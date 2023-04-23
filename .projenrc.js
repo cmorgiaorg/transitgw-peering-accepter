3fe7202e958ee44bf21179883e31ceb47cd13f0d
Claudio Morgia <cmorgia@amazon.ch>
2023-04-23 18:37:38 +0200

    feat(publish): enable Pypi

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,22 @@
 const { awscdk } = require('projen');
 
 const cdkVersion = '2.76.0';
+const projectName = 'newtgw';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Claudio Morgia',
   authorAddress: 'cmorgia@amazon.ch',
   cdkVersion: cdkVersion,
   defaultReleaseBranch: 'main',
-  name: 'newtgw',
-  repositoryUrl: 'https://github.com/cmorgia/newtgw.git',
+  name: projectName,
+  repositoryUrl: `https://github.com/cmorgia/${projectName}.git`,
   deps: [
     `@aws-cdk/aws-lambda-python-alpha@${cdkVersion}-alpha.0`,
   ],
+  publishToPypi: {
+    distName: projectName,
+    module: projectName
+  }
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
