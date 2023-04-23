103638c1cdb638f68209f4abadebd3f63ca43c6f
Claudio Morgia <cmorgia@amazon.ch>
2023-04-23 19:04:45 +0200

    fix(publish): updated config

--- .projenrc.js
+++ .projenrc.js
@@ -15,8 +15,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   ],
   publishToPypi: {
     distName: projectName,
-    module: projectName
-  }
+    module: projectName,
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
