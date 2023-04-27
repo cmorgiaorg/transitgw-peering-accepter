16c43b21c1bab478857eca09b9211997f1f31238
Claudio Morgia <cmorgia@amazon.ch>
2023-04-27 16:52:26 +0200

    fix(publish): bundle lambda code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { ExecSyncOptions, execSync } from 'child_process';
 import * as path from 'path';
 import { CustomResource, Duration, Stack } from 'aws-cdk-lib';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
@@ -9,12 +10,34 @@ import { Construct } from 'constructs';
 export class AcceptTransitPeering extends Construct {
   constructor(scope: Construct, id: string, transitGatewayAttachmentId: string) {
     super(scope, id);
+    const execOptions: ExecSyncOptions = { stdio: ['ignore', process.stderr, 'inherit'] };
 
     const acceptTransitPeeringHandler:Function =
           new Function(this, 'AcceptTransitPeeringHandler', {
             functionName: 'accept-transit-peering-handler',
             runtime: Runtime.PYTHON_3_10,
-            code: Code.fromAsset(path.join(__dirname, '../src')),
+            code: Code.fromAsset(path.join(__dirname, '../src'), {
+              bundling: {
+                image: Runtime.PYTHON_3_10.bundlingImage,
+                local: {
+                  tryBundle(outputDir: string) {
+                    try {
+                      execSync('pip3 --version', execOptions);
+                    } catch {
+                      return false;
+                    }
+                    execSync(`pip3 install -r ${path.join(__dirname, '..', 'src', 'requirements.txt')} -t ${outputDir}`);
+                    execSync(`cp -a ${path.join(__dirname, '..', 'src')} ${outputDir}`);
+
+                    return true;
+                  },
+                },
+                command: [
+                  'bash', '-c',
+                  'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
+                ],
+              },
+            }),
             handler: 'accept-transit-peering-handler.handler',
             memorySize: 1024,
             timeout: Duration.seconds(240),
